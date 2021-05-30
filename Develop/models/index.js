f226a73d21df8aaf30601f15003ab8af9eda05b3
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-30 15:04:11 +1000

    product belongs to many tags

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -21,6 +21,10 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
